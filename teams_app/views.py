b759050e5aefcef9d908116be9972ef117acde03
Maxim V <m4k51m15k4m@gmail.com>
2024-06-24 18:41:41 +0300

    add views.py

--- teams_app/views.py
+++ teams_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Team, Person
+from .serializers import TeamSerializer, PersonSerializer
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
