bffae837ec5d8bdb638153f134086132bd9d51f9
Maxim V <m4k51m15k4m@gmail.com>
2024-06-24 18:40:11 +0300

    complete models.py

--- teams_app/models.py
+++ teams_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    team = models.ForeignKey(
+        Team, related_name="members", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
